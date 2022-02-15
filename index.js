649bde8ea19f86fa7532928799ca485b02e0538f
BimoSora <bimosora3@gmail.com>
2022-02-15 11:45:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1174,16 +1174,15 @@ bot.command('unbanchat', async(ctx) => {
 
 //saving documents to db and generating link
 bot.on('document', async(ctx, next) => {
-
     await new Promise((resolve, reject) => {
         setTimeout(() => {
           return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2)){
-            let document = ctx.message.document
+            document = ctx.message.document
 
             if(ctx.message.media_group_id == undefined){
                 var tag = `✔️ Document save`;
@@ -1246,11 +1245,11 @@ bot.on('document', async(ctx, next) => {
                         reply_to_message_id: ctx.message.message_id
                     })
                     await ctx.replyWithDocument(document.file_id, {
-                        chat_id: logchannel,
+                        chat_id: process.env.LOG_CHANNEL,
                         caption: `${tag} \n<b>From:</b> ${ctx.from.id}\n<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Name file:</b> ${file_name2}\n<b>Size:</b> ${document.file_size} B\n<b>File ID:</b> ${document.file_unique_id} ${mediaId} \n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${document.file_unique_id} ${mediaId2} ${caption2}`,
                         parse_mode:'HTML'
                     })
-                    let fileDetails1 = {
+                    fileDetails1 = {
                         file_name: file_name2,
                         userId:ctx.from.id,
                         file_id: document.file_id,
@@ -1270,16 +1269,15 @@ bot.on('document', async(ctx, next) => {
 
 //video files
 bot.on('video', async(ctx, next) => {
-
     await new Promise((resolve, reject) => {
         setTimeout(() => {
         return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2)){
-            let video = ctx.message.video
+            video = ctx.message.video
     
             if(ctx.message.media_group_id == undefined){
                 var tag = `✔️ Video save`;
@@ -1342,11 +1340,11 @@ bot.on('video', async(ctx, next) => {
                         reply_to_message_id: ctx.message.message_id
                     })
                     await ctx.replyWithVideo(video.file_id, {
-                        chat_id: logchannel,
+                        chat_id: process.env.LOG_CHANNEL,
                         caption: `${tag} \n<b>From:</b> ${ctx.from.id}\n<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Name file:</b> ${file_name2}\n<b>Size:</b> ${video.file_size} B\n<b>File ID:</b> ${video.file_unique_id} ${mediaId} \n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id} ${mediaId2} ${caption2}`,
                         parse_mode:'HTML'
                     })
-                    let fileDetails1 = {
+                    fileDetails1 = {
                         file_name: file_name2,
                         userId:ctx.from.id,
                         file_id: video.file_id,
@@ -1366,16 +1364,15 @@ bot.on('video', async(ctx, next) => {
 
 //photo files
 bot.on('photo', async(ctx, next) => {
-
     await new Promise((resolve, reject) => {
         setTimeout(() => {
           return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
         if(ctx.from.id == Number(process.env.ADMIN) || ctx.from.id == Number(process.env.ADMIN1) || ctx.from.id == Number(process.env.ADMIN2)){
-            let photo = ctx.message.photo[1]
+            photo = ctx.message.photo[1]
 
             if(ctx.message.media_group_id == undefined){
                 var tag = `✔️ Photo save`;
@@ -1438,11 +1435,11 @@ bot.on('photo', async(ctx, next) => {
                         reply_to_message_id: ctx.message.message_id
                     })
                     await ctx.replyWithPhoto(photo.file_id, {
-                        chat_id: logchannel,
+                        chat_id: process.env.LOG_CHANNEL,
                         caption: `${tag} \n<b>From:</b> ${ctx.from.id}\n<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Name file:</b> ${file_name2}\n<b>Size:</b> ${photo.file_size} B\n<b>File ID:</b> ${photo.file_unique_id} ${mediaId} \n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo.file_unique_id} ${mediaId2} ${caption2}`,
                         parse_mode:'HTML'
                     })
-                    let fileDetails1 = {
+                    fileDetails1 = {
                         file_name: file_name2,
                         userId:ctx.from.id,
                         file_id: photo.file_id,
