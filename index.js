2555d00b50437bf9150ee37aee50e885c92cfe86
BimoSora <bimosora3@gmail.com>
2022-03-05 22:02:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1182,7 +1182,7 @@ bot.on('document', async(ctx, next) => {
     await new Promise((resolve, reject) => {
         setTimeout(() => {
           return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
@@ -1279,7 +1279,7 @@ bot.on('video', async(ctx, next) => {
     await new Promise((resolve, reject) => {
         setTimeout(() => {
         return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
@@ -1376,7 +1376,7 @@ bot.on('photo', async(ctx, next) => {
     await new Promise((resolve, reject) => {
         setTimeout(() => {
           return resolve("Result");
-        }, 1_000);
+        }, 2_000);
     });
 
     if(ctx.chat.type == 'private') {
