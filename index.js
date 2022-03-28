58bb4238ba42f458739dd19dd6650ce028f82fa0
BimoSora <bimosora3@gmail.com>
2022-03-28 13:40:42 +0700

    first commit

--- index.js
+++ index.js
@@ -1469,7 +1469,9 @@ bot.on('photo', async(ctx,next) => {
 bot.command('test',async(ctx)=>{
     const users = [ctx];
     const userIds = users.map(user => ctx.from.id)
-    await bot.telegram.sendMessage(userIds,`Percobaan berhasil`)
+    const array = Object.entries(userIds);
+    const objFromArray = Object.fromEntries(array);
+    await bot.telegram.sendMessage(objFromArray,`Percobaan berhasil`)
 })
 
 bot.command('stats',async(ctx)=>{
