1dd22955ce5769a9e9d8e8813b946ca5047a164a
BimoSora <bimosora3@gmail.com>
2022-03-28 13:32:13 +0700

    first commit

--- index.js
+++ index.js
@@ -1469,7 +1469,7 @@ bot.on('photo', async(ctx,next) => {
 bot.command('test',async(ctx)=>{
     const users = [ctx];
     const userIds = users.map(user => ctx.from.id)
-    await ctx.reply(`Percobaan berhasil`{
+    await ctx.reply(`Percobaan berhasil`,{
         chat_id: userIds
     })
 })
