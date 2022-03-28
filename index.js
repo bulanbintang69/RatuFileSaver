8e2bf838d3b93ac6850b1822470a22f81c77b970
BimoSora <bimosora3@gmail.com>
2022-03-28 14:46:56 +0700

    first commit

--- index.js
+++ index.js
@@ -1441,13 +1441,12 @@ bot.on('photo', async(ctx) => {
                     })
                 }else{
                     await bot.telegram.sendPhoto(users, photo.file_id, {
-                        chat_id: ctx.chat.id,
                         caption: `${tag} \n<b>Name file:</b> ${file_name2}\n<b>Size:</b> ${photo.file_size} B\n<b>File ID:</b> ${photo.file_unique_id} ${mediaId} \n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo.file_unique_id} ${mediaId2}`,
                         parse_mode: 'HTML',
                         disable_web_page_preview: true,
                         reply_to_message_id: ctx.message.message_id
                     })
-                    await bot.telegram.sendPhoto(process.env.LOG_CHANNEL,photo.file_id, {
+                    await bot.telegram.sendPhoto(process.env.LOG_CHANNEL, photo.file_id, {
                         caption: `${tag} \n<b>From:</b> ${ctx.from.id}\n<b>Name:</b> <a href="tg://user?id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Name file:</b> ${file_name2}\n<b>Size:</b> ${photo.file_size} B\n<b>File ID:</b> ${photo.file_unique_id} ${mediaId} \n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo.file_unique_id} ${mediaId2} ${caption2}`,
                         parse_mode:'HTML'
                     })
