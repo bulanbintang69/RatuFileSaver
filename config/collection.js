fc3b14f0f0792117cf4e820c51d7aeb1ece4cc31
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2022-05-05 05:51:33 +0700

    Update collection.js

--- config/collection.js
+++ config/collection.js
@@ -1,5 +1,5 @@
 module.exports={
-    FILE_COLLECTION : 'RatuFileBackup',
+    FILE_COLLECTION : 'menfes2001',
     USER_COLLECTION : 'user',
     BANNED_COLLECTION :'banned',
     GROUP_COLLECTION :'group'
